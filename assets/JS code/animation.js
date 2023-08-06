4ff997487c7786a48aec270dc6655a282d3aadd3
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-08-06 19:54:34 +1000

    updateed animation.js

--- assets/JS code/animation.js
+++ assets/JS code/animation.js
@@ -1,6 +1,4 @@
-// animations.js
-
-// Array of summon animation file names
+// Array of summon animation files
 const summonAnimations = [
   'FGO.mp4',
   'Nikke.mp4',
@@ -16,6 +14,41 @@ const summonAnimations = [
 function getRandomSummonAnimation() {
   const randomIndex = Math.floor(Math.random() * summonAnimations.length);
   return summonAnimations[randomIndex];
-};
+}
 
-export { summonAnimations, getRandomSummonAnimation };
+// Function to play the summon animation
+function playSummonAnimation(callback) {
+  const animationFile = getRandomSummonAnimation();
+  const animationPath = `./assets/Summon/${animationFile}`;
+  const summonAnimations = document.getElementById('summon-animation');
+  const skipButton = document.getElementById('skip-button');
+
+  summonAnimations.src = animationPath;
+  summonAnimations.style.display = 'block'; // Show the video element
+  summonAnimations.play();
+
+  console.log(`Playing animation: ${animationFile}`); // Log the animation being played
+
+  summonAnimations.addEventListener('play', () => {
+    if (skipButton) skipButton.style.display = 'block'; // Show the "Skip" button during the summon animation
+  });
+
+  function skipAnimation() {
+    summonAnimations.pause(); // Pause the animation
+    summonAnimations.style.display = 'none'; // Hide the summon animation
+    skipButton.style.display = 'none'; // Hide the "Skip" button
+    callback(); // Invoke the callback function after animation finishes
+    skipButton.removeEventListener('click', skipAnimation);
+  }
+
+  skipButton.addEventListener('click', skipAnimation);
+
+  summonAnimations.addEventListener('ended', function () {
+    skipButton.removeEventListener('click', skipAnimation);
+    summonAnimations.style.display = 'none'; // Hide the summon animation
+    skipButton.style.display = 'none'; // Hide the "Skip" button after the summon animation.
+    callback(); // Invoke the callback function after animation finishes
+  });
+}
+
+export { summonAnimations, playSummonAnimation, getRandomSummonAnimation };
