9a0ec540cac69f89e08c47de8c3d8da43668d479
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-08-08 00:36:06 +1000

    added clearCharacterContainer

--- assets/JS code/animation.js
+++ assets/JS code/animation.js
@@ -51,4 +51,29 @@ function playSummonAnimation(callback) {
   });
 }
 
-export { summonAnimations, playSummonAnimation, getRandomSummonAnimation };
+// Function to save the summoned characters to local storage
+function saveCharactersLStorage() {
+  const summonedCharacters = [];
+  const characterCards = document.querySelectorAll('.character-card');
+
+  characterCards.forEach(characterCard => {
+    const characterImage = characterCard.querySelector('.character-image');
+    summonedCharacters.push({
+      image: characterImage.src,
+      name: characterImage.alt,
+      rarity: parseInt(characterImage.getAttribute('data-rarity'))
+    });
+  });
+
+  localStorage.setItem('summonedCharacters', JSON.stringify(summonedCharacters));
+  console.log('Summoned characters saved to local storage:', summonedCharacters);
+}
+
+// Function to clear the character container
+function clearCharacterContainer() {
+  const characterContainer = document.getElementById('character-container');
+  characterContainer.innerHTML = '';
+}
+
+
+export { summonAnimations, playSummonAnimation, getRandomSummonAnimation, clearCharacterContainer };
